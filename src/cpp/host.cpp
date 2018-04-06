ae30740022d0b366d6a8a2d2e1c53d3147ed54ea
Cherie Williams <cherie@mayfieldrobotics.com>
2018-04-06 10:43:33 -0700

    Add debug message for server hostname

--- src/cpp/host.cpp
+++ src/cpp/host.cpp
@@ -766,6 +766,7 @@ bool Host::Service::set_ice_servers(
 
     for (auto i = 0; i < req.ice_servers.size(); i++) {
         webrtc::PeerConnectionInterface::IceServer server;
+        ROS_DEBUG("Adding ice server %s", server.uri.c_str());
         server.uri = req.ice_servers[i].uri;
         server.username = req.ice_servers[i].username;
         server.password = req.ice_servers[i].password;
